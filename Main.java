082617db11a5ff123c7c424744e5c8b669a7896f
samamel3119 <samuelyepes502@gmail.com>
2026-09-16 14:48:44 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package Ejercicio1;
-
-public class Main {
-    public static void main(String[] args) {
-        operation operation = new operation();
-        operation.mostrarResultados();
-    }
-}
