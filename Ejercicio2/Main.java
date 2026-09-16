e503069ef9c25fac529f2c9d8febd1b47292f402
samamel3119 <samuelyepes502@gmail.com>
2026-09-16 14:48:07 -0500

    Add files via upload

---
+++ Ejercicio2/Main.java
@@ -0,0 +1,8 @@
+package Ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        operation operation = new operation();
+        operation.mostrarResultados();
+    }
+}
--- Ejercicio2/operation.java
+++ Ejercicio2/operation.java
@@ -1,1 +1,24 @@
+package Ejercicio2;
 
+public class operation {
+    double suma = 0;
+    double x = 20;
+    double y = 40;
+public operation () {
+    calcularSuma();
+    calcularCuadrado();
+    totalSuma();
+}
+public void calcularSuma(){
+        suma += x;
+}
+public void calcularCuadrado(){
+    x += Math.pow(y, 2);
+}
+public void totalSuma(){
+    suma += x/y;
+    }
+public void mostrarResultados() {
+        System.out.println("El valor de la suma es:" + suma);
+    }
+}
