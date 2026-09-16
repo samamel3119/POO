291e8d2df5e720175405684338f3d5949929464b
samamel3119 <samuelyepes502@gmail.com>
2026-09-16 14:51:29 -0500

    Add files via upload

---
+++ Ejercicio3/Main.java
@@ -0,0 +1,8 @@
+package Ejercicio3;
+
+public class Main {
+    static void main(String[] args) {
+        operation operation = new operation();
+        operation.mostrarResultados();
+    }
+}
--- Ejercicio3/operation.java
+++ Ejercicio3/operation.java
@@ -1,1 +1,29 @@
+package Ejercicio3;
 
+public class operation {
+    double HorasTrabajadas = 48;
+    double ValorHora = 5000;
+    double retencion = 12.5;
+    double porcentRetefuente = retencion / 100;
+    double SalarioBruto, ValorRetefuente, salarioNeto;
+
+    public operation() {
+        calcSalarioBruto();
+        calcValorRete();
+        calcSalarioNeto();
+    }
+    public void calcSalarioBruto(){
+        this.SalarioBruto = this.HorasTrabajadas * this.ValorHora;
+    }
+    public void calcValorRete(){
+        this.ValorRetefuente = this.porcentRetefuente * this.SalarioBruto;
+    }
+    public void calcSalarioNeto(){
+        this.salarioNeto = this.SalarioBruto - this.ValorRetefuente;
+    }
+    public void mostrarResultados(){
+        System.out.println("Salario Bruto: " + SalarioBruto);
+        System.out.println("Retencion en la fuente: " + ValorRetefuente);
+        System.out.println("Salario Neto: " + salarioNeto);
+    }
+}
